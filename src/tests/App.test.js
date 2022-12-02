a0e500a4e549c44d6d07a3830f43f19519a219e3
Tarcisio R Sacramento <tarcisacra333@gmail.com>
2022-12-02 01:15:24 -0400

    Conclui requisito N°5

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,9 +1,39 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import {render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import App from '../App';
 
-test('I am your test', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/Hello, App!/i);
-  expect(linkElement).toBeInTheDocument();
-});
+describe("Test a Força", () => {  
+  test("testa inputs", () => {
+    render(<App />)
+    expect(screen.getByTestId('name-filter')).toBeInTheDocument()
+    expect(screen.getByTestId('column-filter')).toBeInTheDocument()
+    expect(screen.getByTestId('comparison-filter')).toBeInTheDocument()
+    expect(screen.getByTestId('value-filter')).toBeInTheDocument()
+    expect(screen.getByTestId('button-filter')).toBeInTheDocument()
+    expect(screen.getByTestId('button-remove-filters')).toBeInTheDocument()
+    expect(screen.getByTestId('column-sort')).toBeInTheDocument()
+    expect(screen.getByTestId('column-sort-input-asc')).toBeInTheDocument()
+    expect(screen.getByTestId('column-sort-input-desc')).toBeInTheDocument()
+    expect(screen.getByTestId('column-sort-button')).toBeInTheDocument()
+    
+    const btn = screen.getByTestId('button-filter')
+    expect(screen.getAllByRole('option', {name: 'population'})[0].selected).toBe(true);
+    const valorDigitado = screen.getByTestId('value-filter')
+    
+
+    let columnFilter = screen.getByTestId('column-filter');
+    expect(columnFilter).toHaveLength(5);
+
+    const comparisonFilter = screen.getByTestId('comparison-filter');
+
+    userEvent.selectOptions(columnFilter, 'diameter');
+    userEvent.selectOptions(comparisonFilter, 'maior que');
+    userEvent.type(valorDigitado, '10000');
+    userEvent.click(btn);
+
+    let filters = screen.getAllByTestId('filter');
+    expect(filters).toHaveLength(1);
+  })
+
+})
